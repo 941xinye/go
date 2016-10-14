17fe33fe3d24c51cd072812017be1722944ccf4d
yexin <yex@befound.cn>
2016-10-14 17:02:59 +0800

    .

--- src/RESTful/main.go
+++ src/RESTful/main.go
@@ -9,9 +9,6 @@ import (
 
 func init() {
 	orm.RegisterDriver("mysql", orm.DRMySQL)
-	// orm.RegisterDataBase("default", "mysql", "root:@/seven_day?charset=utf8")
-	// orm.RegisterDataBase("default", "mysql", "restful:Uft8ibTYTXE2u7oT@115.28.241.202/seven_day?charset=utf8")
-	// [username[:password]@][protocol[(address)]]/dbname[?param1=value1&...&paramN=valueN]
 	orm.RegisterDataBase("default", "mysql", beego.AppConfig.String(beego.AppConfig.String("runmode")+"::mysqluser")+":"+beego.AppConfig.String(beego.AppConfig.String("runmode")+"::mysqlpass")+"@"+beego.AppConfig.String(beego.AppConfig.String("runmode")+"::mysqlurls")+"/"+beego.AppConfig.String(beego.AppConfig.String("runmode")+"::mysqldb")+"?charset=utf8")
 }
 
