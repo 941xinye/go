70c8f1de73964fea3541cd0319ff318aaf90a3fe
yexin <yex@befound.cn>
2017-01-17 11:17:03 +0800

    .

--- src/HelloWorld/hello.go
+++ src/HelloWorld/hello.go
@@ -5,5 +5,21 @@ import (
 )
 
 func main() {
-	fmt.Println("Say Hello")
+	var a [5]int
+	fmt.Println(a)
+	a[4] = 9
+	fmt.Println(a)
+	fmt.Println(a[4])
+	fmt.Println(len(a))
+
+	b := [5]int{1, 2, 3, 4, 5}
+	fmt.Println(b)
+
+	var c [2][3]int
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 3; j++ {
+			c[i][j] = i + j
+		}
+	}
+	fmt.Println(c)
 }
