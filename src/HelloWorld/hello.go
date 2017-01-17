ce3c23d040dbab55a2016dedc36c367e53ea591e
xinye <3030944332@qq.com>
2017-01-17 11:18:37 +0800

    .

--- src/HelloWorld/hello.go
+++ src/HelloWorld/hello.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+    "net/http"
 	"fmt"
 )
 
+func hello(w http.ResponseWriter,req *http.Request){
+    fmt.Println("Say H");
+}
+
 func main() {
-	fmt.Println("Say Hello")
+    http.HandleFunc("/",hello)  
+  
+    //服务器要监听的主机地址和端口号  
+    err := http.ListenAndServe("127.0.0.1:8081", nil)  
+  
+    if err != nil {  
+        fmt.Println("ListenAndServe error: ", err.Error())  
+    }  
 }
